63aa2cc0c1ffe8462dc7d1328d45a45b0430873a
Jearvon Dharrie <j.dharrie@gmail.com>
2015-06-08 21:38:06 -0400

    Display contacts

--- src/cljs/pplz/core.cljs
+++ src/cljs/pplz/core.cljs
@@ -2,14 +2,31 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state (atom {:contacts [
+                                     {:first "Vincent" :last "Chase"}
+                                     {:first "Ari" :last "Gold"}]}))
+
+(defn contact-view
+  [contact owner]
+  (reify
+    om/IRender
+    (render
+      [this]
+      (dom/tr nil
+       (dom/td nil (:first contact))
+       (dom/td nil (:last contact))))))
+
+(defn contacts-view
+  [contacts owner]
+  (reify
+    om/IRender
+    (render
+      [this]
+      (apply dom/table nil
+             (om/build-all contact-view (:contacts contacts))))))
 
 (defn main []
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app)))))
-    app-state
-    {:target (. js/document (getElementById "app"))}))
+   contacts-view app-state
+   {:target (. js/document (getElementById "app"))}))
+

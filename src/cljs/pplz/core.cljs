6c5df9493c55d951e12934c1a0340b562cf650a9
Jearvon Dharrie <j.dharrie@gmail.com>
2015-06-10 11:21:24 -0400

    Add the ability to delete a contact

--- src/cljs/pplz/core.cljs
+++ src/cljs/pplz/core.cljs
@@ -1,32 +1,61 @@
 (ns pplz.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (defonce app-state (atom {:contacts [
                                      {:first "Vincent" :last "Chase"}
-                                     {:first "Ari" :last "Gold"}]}))
+                                     {:first "Ari" :last "Gold"}
+                                     {:first "Johnny" :last "Drama"}]}))
 
 (defn contact-view
   [contact owner]
   (reify
-    om/IRender
-    (render
-      [this]
+    om/IRenderState
+    (render-state
+      [this {:keys [delete]}]
       (dom/tr nil
-       (dom/td nil (:first contact))
-       (dom/td nil (:last contact))))))
+              (dom/td nil (:first contact))
+              (dom/td nil (:last contact)
+                      (dom/td nil
+                              (dom/button #js {:onClick
+                                               (fn [e] (put! delete @contact))} "Delete")))))))
 
 (defn contacts-view
   [contacts owner]
   (reify
-    om/IRender
-    (render
-      [this]
-      (apply dom/table nil
-             (om/build-all contact-view (:contacts contacts))))))
+    om/IInitState
+    (init-state
+      [_]
+      {:delete (chan)})
+    om/IWillMount
+    (will-mount
+      [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop
+              []
+              (let [contact (<! delete)]
+                (om/transact! contacts :contacts
+                              (fn
+                                [xs]
+                                (vec (remove #(= contact %) xs))))
+                (recur))))))
+    om/IRenderState
+    (render-state
+      [this {:keys [delete]}]
+      (dom/table nil
+                 (dom/thead nil
+                            (dom/tr nil
+                                    (dom/td nil "First")
+                                    (dom/td nil "Last")
+                                    (dom/td nil "Actions")))
+                 (apply dom/tbody nil
+                        (om/build-all contact-view (:contacts contacts)
+                                      {:init-state {:delete delete}}))))))
 
 (defn main []
   (om/root
-   contacts-view app-state
-   {:target (. js/document (getElementById "app"))}))
+    contacts-view app-state
+    {:target (. js/document (getElementById "app"))}))
 
